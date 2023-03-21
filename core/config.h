b21a10be29f364a3f6d331d3a985c973e0dcd14a
Geibinger <friedl.jak@gmail.com>
2023-03-21 10:59:28 +0100

    updated config and doxigen support

--- core/config.h
+++ core/config.h
@@ -1,38 +1,63 @@
-//--------------------------hardware declarations---------------------------------
-// measurements in [m]
-#define WHEELRADIUS 0.075 // radius of wheels
-#define L_X 0.38 // distance between wheel contact point in x direction
-#define L_Y 0.32 // distance between wheel contact point in y direction
+/**
+ * @file config.h
+ * @author Friedl Jakob (friedl.jak@gmail.com)
+ * @brief Configuration file for the Roboost firmware.
+ * @version 0.1
+ * @date 2023-03-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 
+
+
+
+/**
+ * @brief Selection of robot kinematics.
+ * MECANUM_4WHEEL: Kinematics of a four wheeled mecanum drive.
+ * SWERVE_3WHEEL: Kinematics of a robot with three swerve drives.
+ */
+#define MECANUM_4WHEEL
+//#define SWERVE_3WHEEL
+
+
+#ifdef MECANUM_4WHEEL
+    /**
+    * @brief Definitions of hardware parameters. Leghts in meters, angles in degree.
+    * 
+    */
+    #define WHEELRADIUS 0.075 // radius of wheels
+    #define L_X 0.38 // distance between wheel contact point in x direction
+    #define L_Y 0.32 // distance between wheel contact point in y direction
 //--------------------------pinout definitions------------------------------------
-// motor back right
-#define M_BR_CCW 33
-#define M_BR_CW 32
-#define M_BR_PWM 25
+    // motor back right
+    #define M_BR_CCW 33
+    #define M_BR_CW 32
+    #define M_BR_PWM 25
 
-// motor back left
-#define M_BL_CW 26
-#define M_BL_CCW 27
-#define M_BL_PWM 13
+    // motor back left
+    #define M_BL_CW 26
+    #define M_BL_CCW 27
+    #define M_BL_PWM 13
 
-// motor front left
-#define M_FL_CW 23
-#define M_FL_CCW 22
-#define M_FL_PWM 21
+    // motor front left
+    #define M_FL_CW 23
+    #define M_FL_CCW 22
+    #define M_FL_PWM 21
 
-// motor front right
-#define M_FR_CCW 18
-#define M_FR_CW 14
-#define M_FR_PWM 19
+    // motor front right
+    #define M_FR_CCW 18
+    #define M_FR_CW 14
+    #define M_FR_PWM 19
+    // PWM config
+    #define M_BR_PWM_CNL 0
+    #define M_BL_PWM_CNL 0
+    #define M_FR_PWM_CNL 0
+    #define M_FL_PWM_CNL 0
 
-// PWM config
-#define M_BR_PWM_CNL 0
-#define M_BL_PWM_CNL 0
-#define M_FR_PWM_CNL 0
-#define M_FL_PWM_CNL 0
-
-#define M_PWM_FRQ 1000 // Hz
-#define M_PWM_RES 8 // 2^n Bits
+    #define M_PWM_FRQ 1000 // Hz
+    #define M_PWM_RES 8 // 2^n Bits
+#endif
 
 // Uncomment if encoders should be used in the system
 //#define ENCODERS
