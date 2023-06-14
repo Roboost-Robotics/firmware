d9ebd16d32bf6d5b8853fc9b6d64850f9447d6aa
Geibinger <friedl.jak@gmail.com>
2023-06-14 20:33:50 +0200

    added micro-ros integration

--- core/src/core.cpp
+++ core/src/core.cpp
@@ -9,51 +9,86 @@
  * 
  */
 
-#include <ros2arduino.h>
+#include <Arduino.h>
+#include <micro_ros_platformio.h>
 
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include "conf_network.h"
-#include "conf_robot.h"
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
 
-#define PUBLISH_FREQUENCY 2 //hz
+#include <std_msgs/msg/int32.h>
 
-void publishString(std_msgs::String* msg, void* arg)
-{
-  (void)(arg);
+#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
+#error This example is only avaliable for Arduino framework with serial transport.
+#endif
 
-  static int cnt = 0;
-  sprintf(msg->data, "Hello ros2arduino %d", cnt++);
-}
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
 
-class StringPub : public ros2::Node
-{
-public:
-  StringPub()
-  : Node("ros2arduino_pub_node")
-  {
-    ros2::Publisher<std_msgs::String>* publisher_ = this->createPublisher<std_msgs::String>("arduino_chatter");
-    this->createWallFreq(PUBLISH_FREQUENCY, (ros2::CallbackFunc)publishString, nullptr, publisher_);
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+// Error handle loop
+void error_loop() {
+  while(1) {
+    delay(100);
   }
-};
-
-WiFiClient client;
-
-void setup() 
-{
-  WiFi.begin(SSID, SSID_PW);
-  while(WiFi.status() != WL_CONNECTED);
-
-  ros2::init(&client, AGENT_IP, AGENT_PORT);
 }
 
-void loop() 
-{
-  static StringPub StringNode;
-
-  ros2::spin(&StringNode);
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+    msg.data++;
+  }
 }
 
+void setup() {
+  // Configure serial transport
+  Serial.begin(115200);
+  set_microros_serial_transports(Serial);
+  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "micro_ros_platformio_node_publisher"));
+
+  // create timer,
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  msg.data = 0;
+}
+
+void loop() {
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+}
 
 #ifdef ENCODERS
 // Encoder specific definitions and functions
--- sensorboard/src/header.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-#include <FreeRTOS.h>
-#include <ros.h>
-
-#define PWR_IN 36
-#define PWR_LED 13
-#define MTC 18
-
-#define PWR_MAX 12.6
