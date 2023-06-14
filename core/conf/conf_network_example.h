7091a82940a8ee901f4c0058e28ac5e40c513592
Geibinger <friedl.jak@gmail.com>
2023-06-14 15:51:34 +0200

    refactoring

--- core/conf/conf_network_example.h
+++ core/conf/conf_network_example.h
@@ -1,7 +1,7 @@
 #ifndef CONF_NETWORK_H
 #define CONF_NETWORK_H
 
-#define SSID = "your_ssid";
+#define SSID = "test";
 #define SSID_PW = "your_password";
 #define AGENT_IP   "your_adgent_ip" // IP of the ROS2 messages recceiver
 #define AGENT_PORT 2018 //AGENT port number
