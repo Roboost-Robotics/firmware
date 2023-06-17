4e77c7eeb982c712d55760c3e3172519d3964da1
Geibinger <friedl.jak@gmail.com>
2023-06-17 11:29:02 +0200

    added micro-ros-samples

---
+++ core/samples/micro-ros-subscriber.cpp
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+#include <micro_ros_platformio.h>
+
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <geometry_msgs/msg/twist.h> // Include Twist message
+
+#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
+#error This example is only avaliable for Arduino framework with serial transport.
+#endif
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+
+// Declare the subscriber
+rcl_subscription_t subscriber;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+// Error handle loop
+void error_loop() {
+  while(1) {
+    delay(100);
+  }
+}
+
+// Create the subscriber callback
+void subscriber_callback(const void * msgin) {
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
+  // Do something with the received message
+  // For example, print the received velocities
+  Serial.print("Received linear velocity: ");
+  Serial.println(msg->linear.x);
+  Serial.print("Received angular velocity: ");
+  Serial.println(msg->angular.z);
+}
+
+void setup() {
+  // Configure serial transport
+  Serial.begin(115200);
+  set_microros_serial_transports(Serial);
+  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
+
+  // create subscriber
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "command_vel"));  // Here we use the name "command_vel"
+
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator)); // Note that we now have 1 handle for the subscriber
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscriber_callback, ON_NEW_DATA));
+}
+
+void loop() {
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+}
