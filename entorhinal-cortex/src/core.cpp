73ac206493545c9d288d87224bff4d64a2d2629b
Geibinger <friedl.jak@gmail.com>
2023-08-01 21:59:55 +0200

    added basic scan data publisher

--- entorhinal-cortex/src/core.cpp
+++ entorhinal-cortex/src/core.cpp
@@ -11,48 +11,148 @@
 
 #include <Arduino.h>
 #include <RPLidar.h>
+#include <micro_ros_platformio.h>
+
+#include "rcl_checks.h"
+#include <rcl/rcl.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
+
+#include <sensor_msgs/msg/laser_scan.h>
+
+#include "conf_network.h"
+#include "conf_robot.h"
 
 #define RPLIDAR_MOTOR 18
 #define RPLIDAR_RX 16
 #define RPLIDAR_TX 17
 
-HardwareSerial RPLidarSerial(2);  // Using HardwareSerial 2
+HardwareSerial RPLidarSerial(2); // Using HardwareSerial 2
 RPLidar lidar;
 
-void setup() {
-  // Initialize serial and lidar
-  Serial.begin(115200);
-  RPLidarSerial.begin(115200, SERIAL_8N1, RPLIDAR_RX, RPLIDAR_TX);
+rcl_publisher_t publisher;
+sensor_msgs__msg__LaserScan scan;
 
-  // Start the lidar's motor
-  pinMode(RPLIDAR_MOTOR, OUTPUT);
-  digitalWrite(RPLIDAR_MOTOR, HIGH);
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
 
-  lidar.begin(RPLidarSerial);
-  lidar.startScan();
+void setup()
+{
+    // Initialize serial and lidar
+    Serial.begin(115200);
+    RPLidarSerial.begin(115200, SERIAL_8N1, RPLIDAR_RX, RPLIDAR_TX);
+
+    // Start the lidar's motor
+    pinMode(RPLIDAR_MOTOR, OUTPUT);
+    digitalWrite(RPLIDAR_MOTOR, HIGH);
+
+    lidar.begin(RPLidarSerial);
+    lidar.startScan();
+
+    // Configure micro-ROS
+    IPAddress agent_ip(AGENT_IP);
+    uint16_t agent_port = AGENT_PORT;
+
+    set_microros_wifi_transports(SSID, SSID_PW, agent_ip, agent_port);
+    delay(2000);
+
+    allocator = rcl_get_default_allocator();
+
+    while (rclc_support_init(&support, 0, NULL, &allocator) != RCL_RET_OK)
+    {
+        delay(1000);
+    }
+
+    while (rclc_node_init_default(&node, "lidar_node", "", &support) !=
+           RCL_RET_OK)
+    {
+        delay(1000);
+    }
+
+    while (rclc_publisher_init_default(
+               &publisher, &node,
+               ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, LaserScan),
+               "scan") != RCL_RET_OK)
+    {
+        delay(1000);
+    }
+
+    while (rclc_executor_init(&executor, &support.context, 1, &allocator) !=
+           RCL_RET_OK)
+    {
+        delay(1000);
+    }
+
+    delay(500);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
 }
 
-void loop() {
-  // Check if lidar is open
-  if (lidar.isOpen()) {
-    RPLidarMeasurement measurement;
-    while (lidar.waitPoint()) {
-      measurement = lidar.getCurrentPoint();
+void loop()
+{
+    if (lidar.isOpen())
+    {
+        // Collect all scanned points
+        std::vector<float> ranges;
+        std::vector<float> intensities;
+        RPLidarMeasurement measurement;
 
-      // measurement.angle is already in degrees
-      float angle = measurement.angle;
+        // Set scan parameters
+        scan.angle_min = 0.0;
+        scan.angle_max = 360.0;
+        scan.angle_increment = 1.0;
+        scan.range_min = 0.0;
+        scan.range_max = 10.0;
 
-      // Print the distance at the middle point
-      if (angle >= 180.0 && angle < 181.0) {
-        Serial.print("Distance at 180 degrees: ");
-        Serial.print(measurement.distance);
-        Serial.println("mm");
-      }
+        // Set frame_id
+        scan.header.frame_id.data = (char*)"lidar";
+        scan.header.frame_id.size = strlen(scan.header.frame_id.data);
+        scan.header.frame_id.capacity = scan.header.frame_id.size + 1;
+
+        // TODO: remove ranges.size() fix
+        //? Why exactly does the error occur when not checking the range size?
+        while (lidar.waitPoint() == RESULT_OK && ranges.size() < 360)
+        {
+            measurement = lidar.getCurrentPoint();
+            Serial.println(measurement.angle);
+            ranges.push_back(measurement.distance / 1000.);
+            intensities.push_back(measurement.quality);
+        }
+
+        // Allocate memory for ranges and intensities
+        scan.ranges.size = ranges.size();
+        scan.ranges.capacity = ranges.size();
+        scan.ranges.data = reinterpret_cast<float*>(
+            allocator.allocate(sizeof(float) * ranges.size(), allocator.state));
+
+        scan.intensities.size = intensities.size();
+        scan.intensities.capacity = intensities.size();
+        scan.intensities.data = reinterpret_cast<float*>(allocator.allocate(
+            sizeof(float) * intensities.size(), allocator.state));
+
+        // Copy data
+        for (size_t i = 0; i < ranges.size(); i++)
+        {
+            scan.ranges.data[i] = ranges[i];
+            scan.intensities.data[i] = intensities[i];
+        }
+
+        // Publish the scan data
+        RCSOFTCHECK(rcl_publish(&publisher, &scan, NULL));
+
+        // Free memory
+        allocator.deallocate(scan.ranges.data, allocator.state);
+        allocator.deallocate(scan.intensities.data, allocator.state);
+
+        delay(100);
     }
-  } else {
-    Serial.println("Lidar is not open!");
-  }
+    else
+    {
+        Serial.println("Lidar is not open!");
+    }
 
-  // Wait a bit before the next scan
-  delay(1000);
+    RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+    delay(100);
 }
